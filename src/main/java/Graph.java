992b417b1f05106e1b1fef5107ecf556a1988ed5
fjane <florence.sayavongsa@gmail.com>
2026-06-02 18:18:17 -0400

    Create Graph interface.

---
+++ src/main/java/Graph.java
@@ -0,0 +1,14 @@
+/**
+ * File: Graph.java
+ * Description: This interface includes operations used to construct, manipulate, and traverse a graph data structure.
+ * This interface is currently set up to be used with String vertices and Edge class objects.
+ * Author: Florence Sayavongsa
+ * Student ID: 3056629
+ * Email ID: florence.sayavongsa@student.adelaide.edu.au
+ * AI Tool Used: Y/N (This includes all AI Tools e.g. ChatGPT, Microsoft or Github Copiliot etc... Please leave blank if you do not wish to share this information)
+ * This is my own work as defined by
+ *    the University's Academic Integrity Policy.
+ **/
+public interface Graph {
+
+}
