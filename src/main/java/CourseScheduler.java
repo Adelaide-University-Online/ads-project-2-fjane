51a0977dbf85cd958743b8097e267dc2830ca9dd
fjane <florence.sayavongsa@gmail.com>
2026-06-03 15:36:12 -0400

    Create CourseScheduler class with description.

---
+++ src/main/java/CourseScheduler.java
@@ -0,0 +1,18 @@
+/**
+ * File: CourseScheduler.java
+ * Description: This is the entry to the application. It populates a course schedule based on a course file name and
+ * amount of subjects to be taken concurrently provided by user input. It handles reading in a text file containing
+ * a course's subjects and prerequisites. Course schedule will be printed to screen and written to current working
+ * directory as "CourseSchedule.txt".
+ * Author: Florence Sayavongsa
+ * Student ID: 3056629
+ * Email ID: florence.sayavongsa@student.adelaide.edu.au
+ * AI Tool Used: Y/N (This includes all AI Tools e.g. ChatGPT, Microsoft or Github Copiliot etc... Please leave blank if you do not wish to share this information)
+ * This is my own work as defined by
+ *    the University's Academic Integrity Policy.
+ **/
+
+public class CourseScheduler {
+
+
+}
