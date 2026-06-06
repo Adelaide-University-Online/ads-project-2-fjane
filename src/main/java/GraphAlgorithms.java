286fdb3b34277456af25d8fa2b4a2113f26e63fb
fjane <florence.sayavongsa@gmail.com>
2026-06-06 15:18:54 -0400

    Add separate method to build degrees map.

--- src/main/java/GraphAlgorithms.java
+++ src/main/java/GraphAlgorithms.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 /**
  * File: GraphAlgorithms.java
- * Description: This class is intended to be a collection of algorithms that can be applied to various graph types.
+ * Description: This class is intended to be a collection of algorithms that can be applied to directed graphs.
  * Author: Florence Sayavongsa
  * Student ID: 3056629
  * Email ID: florence.sayavongsa@student.adelaide.edu.au
@@ -11,6 +11,41 @@ import java.util.*;
  *    the University's Academic Integrity Policy.
  **/
 public class GraphAlgorithms {
+    static Map<String, int[]> degrees;
+
+    /**
+     * Creates a map of each vertex and the number of incoming and outgoing edges it has.
+     * @param graph A graph represented as an adjacency list
+     * @return A map of each vertex as a key and its degrees as values
+     */
+    public static Map<String, int[]> degreesMap (AbstractGraph graph) {
+        degrees = new HashMap<>();
+
+        // Initialize with 0 for each vertex
+        for (String vertex : graph.getVertices()) {
+            degrees.put(vertex, new int[]{0,0});
+
+        }
+
+        // Count incoming and outgoing edges for each vertex
+        for (String vertex : graph.getVertices()) {
+            Iterator<Edge> edgeIterator = graph.edgeIterator(vertex);
+
+            // Loop through all edges
+            while (edgeIterator.hasNext()) {
+                Edge edge = edgeIterator.next();
+
+                // Increment source's out-degree
+                degrees.get(vertex)[1]++;
+
+                // Increment destination's in-degree
+                degrees.get(edge.getDestination())[0]++;
+            }
+        }
+
+        return degrees;
+    }
+
     /**
      * This algorithm applies topological sort using Kahn's Breadth-First Search algorithm to arrange vertices of a
      * directed acyclic graph in linear order ensuring ordering does not violate vertex ordering of directed edges.
