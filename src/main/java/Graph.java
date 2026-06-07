dc65af9fa6784c90f4271ff0c78840fd854170c8
fjane <florence.sayavongsa@gmail.com>
2026-06-07 09:52:31 -0400

    Add accessor method for degrees map.

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -1,4 +1,5 @@
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -25,6 +26,9 @@ public interface Graph {
 
     /** Return all the edges in a graph. */
     Set<Edge> getEdgeSet();
+
+    /** Return all vertices and the number of incoming and outgoing edges. */
+    Map<String, int[]> getDegrees();
 
     /** Return true if the graph is directed.  */
     boolean isDirected();
