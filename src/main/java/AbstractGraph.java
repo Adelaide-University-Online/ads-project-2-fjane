3cb3564356e0287b8b9fc866aa79c7c824a39104
fjane <florence.sayavongsa@gmail.com>
2026-06-08 19:50:11 -0400

    Add modifier methods.

--- src/main/java/AbstractGraph.java
+++ src/main/java/AbstractGraph.java
@@ -77,6 +77,47 @@ public abstract class AbstractGraph implements Graph{
         return edges;
     }
 
+    // Modifier methods
+
+    /**
+     * Creates a new Vertex, adds it to the graph's vertices map and the vertices reverse lookup map.
+     * @param id an integer id
+     * @param name a String name
+     */
+    public void addVertex(int id, String name) {
+        // Adds a new Vertex to the vertices map
+        vertices.put(id, new Vertex(id, name));
+
+        // Adds the new Vertex to the vertices reverse look up map
+        nameToId.put(name, id);
+    }
+
+    /**
+     * Creates a new Edge, adds it to the graph's edges map. Verifies that the proposed Edge's endpoints exist in the
+     * graph's vertices map first.
+     * @param sourceId an integer id of a Vertex
+     * @param destinationId an integer id of a Vertex
+     */
+    public void addEdge(int sourceId, int destinationId) {
+        // Checks if the source Vertex exists in the vertice map
+        if(!vertices.containsKey(sourceId)) {
+            throw new NoSuchElementException("Edge source vertex was not found in vertex list.");
+        }
+
+        // Checks if the destination Vertex exists in the vertice map
+        if(!vertices.containsKey(destinationId)) {
+            throw new NoSuchElementException("Edge destination vertex was not found in vertex list.");
+        }
+
+        // Adds new Edge to the edges map
+        edges.computeIfAbsent(sourceId, _ -> new ArrayList<>()).add(new Edge(sourceId, destinationId));
+
+        // Adds new Edge to the edges map in the opposite direction for undirected graphs
+        if(!directed) {
+            edges.computeIfAbsent(destinationId, _ -> new ArrayList<>()).add(new Edge(destinationId, sourceId));
+        }
+    }
+
     /* Class template inspired by:
     Koffman, E.B., & Wolfgang, P.A.T. (2015). Data Structures: Abstraction and Design Using Java: Chapter 10 Graphs.
     (3rd ed.). Wiley. http://ebookcentral.proquest.com/lib/adelaideuni/detail.action?docID=5106355
