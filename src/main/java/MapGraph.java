6aa09fa61082080d9463069abf34177290d3026e
fjane <florence.sayavongsa@gmail.com>
2026-06-09 13:03:47 -0400

    Reset for change in Vertex and Edge data structures including attribute move from AbstractGraph.

--- src/main/java/MapGraph.java
+++ src/main/java/MapGraph.java
@@ -1,10 +1,9 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * File: MapGraph.java
- * Description: This is a graph implementation that uses an adjacency-map to represent String vertices and edges. MapGraph is
- * an extension of the AbstractGraph class.
+ * Description: This is a representation of a graph that uses an adjacency-map to represent vertices and edges.
+ * MapGraph is an extension of the AbstractGraph class.
  * Author: Florence Sayavongsa
  * Student ID: 3056629
  * Email ID: florence.sayavongsa@student.adelaide.edu.au
@@ -13,94 +12,21 @@ import java.util.stream.Collectors;
  *    the University's Academic Integrity Policy.
  **/
 
-public class MapGraph extends AbstractGraph {
+public class MapGraph {
+
     // Attributes
-    private Map<String, Set<Edge>> adjacencyMap;
+    protected Map<Integer, Vertex> vertices;
+    protected Map<Integer, List<Edge>> edges; // Adjacency list
+    protected Map<String, Integer> nameToId; // Reverse look up map for vertices
+    protected Map<Integer, Integer> inDegree; //
+    protected Map<Integer, Integer> outDegree; //
+    protected boolean directed;
+    protected boolean weighted;
 
     // Constructor
-    public MapGraph(Set<String> vertices, Set<Edge> edgeSet, boolean directed, boolean weighted) {
-        super(vertices, new LinkedHashSet<>(), directed, weighted);
-        this.adjacencyMap = new LinkedHashMap<>();
-
-        // Add vertices to adjacency map as keys
-        for (String vertex : vertices) {
-            adjacencyMap.put(vertex, new LinkedHashSet<>());
-        }
-
-        // Add edges to corresponding vertices in adjacency map
-        for (Edge edge : edgeSet) {
-            insert(edge);
-        }
-    }
-
-    /**
-     * Adds new edges to the graph if it doesn't already exist.
-     * @param edge Edge to be added
-     */
-    public void insert(Edge edge) {
-        String source = edge.getSource();
-        String destination = edge.getDestination();
-
-        // Add edge by searching for map key (source vertex)
-        adjacencyMap.putIfAbsent(source, new LinkedHashSet<>());
-        adjacencyMap.get(source).add(edge);
-        this.edgeSet.add(edge);
-
-        // Update degrees map incrementally
-        degrees.putIfAbsent(source, new int[]{0, 0});
-        degrees.putIfAbsent(destination, new int[]{0, 0});
-        degrees.get(source)[1]++;       // source out-degree
-        degrees.get(destination)[0]++;  // destination in-degree
-
-        // Add edge in both directions if graph is undirected
-        if (!directed) {
-            Edge reverseEdge = new Edge(destination, source);
-            adjacencyMap.putIfAbsent(destination, new LinkedHashSet<>());
-            adjacencyMap.get(destination).add(reverseEdge);
-            this.edgeSet.add(reverseEdge);
-
-            // Update degrees map for edges in the opposite direction
-            degrees.get(destination)[1]++;
-            degrees.get(source)[0]++;
-        }
-    }
-
-    /**
-     * Returns a String representation of an adjacency map. Vertices are listed one per line with any connected vertices
-     * to the right such that a vertex with multiple outgoing edges would be represented in the following format.
-     * source vertex: destination vertex, different destination vertex
-     */
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-
-        // Add each map entry to StringBuilder one at a time
-        for (Map.Entry<String, Set<Edge>> entry : adjacencyMap.entrySet()) {
-            // Source vertex
-            sb.append(entry.getKey()).append(": ");
-
-            // Add destination vertices to the right
-            String destinations = entry.getValue().stream()
-                    .map(Edge::getDestination)
-                    .collect(Collectors.joining(", "));
-            sb.append(destinations).append("\n");
-        }
-        return sb.toString();
-    }
-
-    /**
-     * Returns an iterator over all vertices connected to a given vertex.
-     * @param source The source vertex
-     * @return all given vertex's neighbours
-     */
-    @Override
-    public Iterator<Edge> edgeIterator(String source) {
-        // Check to see if vertex exists in map
-        if (!adjacencyMap.containsKey(source)) {
-            throw new NoSuchElementException("Vertex not found: " + source);
-        }
-
-        return adjacencyMap.get(source).iterator();
+    public MapGraph(boolean directed, boolean weighted) {
+        this.directed = directed;
+        this.weighted = weighted;
     }
 
 }
