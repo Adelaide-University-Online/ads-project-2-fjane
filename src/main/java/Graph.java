eaedf2bd2111db9090450d56fc0220ad51c7e07f
fjane <florence.sayavongsa@gmail.com>
2026-06-10 08:42:19 -0400

    Add methods to check if a Vertex or Edge exists in a graph.

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -47,6 +47,12 @@ public interface Graph {
     /** Add a new Edge to the graph. */
     void addEdge(int sourceId, int destinationId);
 
+    /** Checks if a Vertex exists in a graph. */
+    void isVertex(int id, String name);
+
+    /** Checks if an Edge exists in a graph. */
+    void isEdge(int sourceId, int destinationId);
+
     /** Return an iterator to the outgoing edges of a vertex.
      * @param source The source vertex
      */
