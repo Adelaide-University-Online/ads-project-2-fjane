a51ee492fef13268cc5844156b6ac36cca66dba6
fjane <florence.sayavongsa@gmail.com>
2026-06-10 14:45:00 -0400

    Update getVertexIdByName method name.

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -33,7 +33,7 @@ public interface Graph {
     Map<Integer, Integer> getOutDegrees();
 
     /** Return a vertex ID by searching for vertex name. */
-    int getVertexId(String name);
+    int getVertexIdByName(String name);
 
     /** Return true if the graph is directed.  */
     boolean isDirected();
