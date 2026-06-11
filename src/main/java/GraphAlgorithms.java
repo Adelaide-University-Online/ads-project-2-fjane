47157cc415b93849e18a627092b3fe7f3546f7b1
fjane <florence.sayavongsa@gmail.com>
2026-06-11 10:44:03 -0400

    Add greedy first-fit method.

--- src/main/java/GraphAlgorithms.java
+++ src/main/java/GraphAlgorithms.java
@@ -103,4 +103,84 @@ public class GraphAlgorithms {
     Geeks for Geeks. (2025, July 23). Longest Path in a Directed Acyclic Graph.
     https://www.geeksforgeeks.org/dsa/find-longest-path-directed-acyclic-graph/
     */
+
+    /**
+     * Assigns vertices to bins using greedy first-fit bin packing. Using a combination of topological ordering,
+     * ascending longest path chain, and descending out-degreee vertices are placed in the earliest open bin while
+     * respecting any dependencies.
+     * @param topoOrder List of vertices sorted using Kahn's algorithm
+     * @param graph A directed acyclic graph
+     * @param longest Map of each vertex (vertexId) to its longest prerequisite chain length
+     * @param binSize Number of vertices allowed in each bin
+     * @return A TreeMap of numbered bins and lists of vertices assigned to each
+     */
+    public static Map<Integer, List<Integer>> greedyBinPack(
+            List<Integer> topoOrder,
+            MapGraph graph,
+            Map<Integer, Integer> longest,
+            int binSize) {
+
+        // Get graph's outdegree map - prioritizes vertices with more dependents
+        Map<Integer, Integer> outDegree = new HashMap<>(graph.getOutDegrees());
+
+        // Sort by ascending critical path length first, then by descending out-degree
+        topoOrder.sort(Comparator
+                .comparingInt((Integer c) -> longest.get(c))
+                .thenComparingInt((Integer c) -> -outDegree.get(c))
+        );
+
+        // Return results in ascending bin order
+        Map<Integer, List<Integer>> levelMap = new TreeMap<>();
+
+        // Track which bin a vertex has been placed in - to ensure prerequisites are placed in an earlier bin
+        Map<Integer, Integer> assignedTerm = new HashMap<>();
+
+        // Loop through sorted vertices (EDF order based on critical path length)
+        for (Integer course : topoOrder) {
+            // Minimum bin
+            int minBin = 1;
+
+            Map<Integer, Vertex> vertices = new HashMap<>(graph.getVertices());
+
+            // Find all prerequisites of a vertex and increase minBin for each found
+            for (Map.Entry<Integer, Vertex> vertex : vertices.entrySet()) {
+                Iterator<Edge> it = graph.edgeIterator(vertex.getKey());
+
+                // Loop through all out-going edges of the current vertex
+                while (it.hasNext()) {
+
+                    // Check if vertex is a prerequisite of the current vertex
+                    if (it.next().getDestination() == (course)) {
+
+                        // Find the bin containing the prerequisite
+                        Integer prereqBin = assignedTerm.get(vertex.getKey());
+
+                        if (prereqBin != null) {
+                            // Assign current vertex to a bin at least one higher than its prerequisite
+                            minBin = Math.max(minBin, prereqBin +1);
+                        }
+                    }
+                }
+            }
+
+            // First-Fit placement. Start from earliest valid bin and scan forward until a bin with availability is found
+            int bin = minBin;
+
+            // getOrDefault - null check protection, returns an empty list if term hasn't been created yet
+            while (levelMap.getOrDefault(bin, Collections.emptyList()).size() >= binSize) {
+                bin++; // Move to next term if current is at full capacity
+            }
+
+            // Add vertex to the selected term and term tracker
+            assignedTerm.put(course, bin);
+            levelMap.computeIfAbsent(bin, _ -> new ArrayList<>()).add(course);
+        }
+
+        return levelMap;
+    }
+    /* Code inspired by:
+    Geeks for Geeks. (2024, December 2). Bin Packing Problem (Minimize number of used Bins).
+    https://www.geeksforgeeks.org/dsa/bin-packing-problem-minimize-number-of-used-bins/
+    */
+
 }
