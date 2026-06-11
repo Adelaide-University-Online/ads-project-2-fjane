0eca640516899eb6bed6a0a358c38acf9e25b298
fjane <florence.sayavongsa@gmail.com>
2026-06-11 11:01:22 -0400

    Update tests for Vertex and Edge type change.

--- src/main/java/GraphAlgorithmsTest.java
+++ src/main/java/GraphAlgorithmsTest.java
@@ -6,60 +6,47 @@ import java.util.*;
 
 class GraphAlgorithmsTest {
 
-    protected Set<String> vertexSet;
-    protected Set<Edge> edgeSet;
-    protected AbstractGraph mapDirected;
-    protected AbstractGraph mapUndirected;
+    protected MapGraph mapDirected;
 
     @BeforeEach
     void setUp() {
-        vertexSet = new LinkedHashSet<>();
-        vertexSet.add("A");
-        vertexSet.add("B");
-        vertexSet.add("C");
-        vertexSet.add("D");
-        vertexSet.add("E");
+        mapDirected = new MapGraph(true);
 
-        edgeSet = new HashSet<>();
-        edgeSet.add(new Edge("A", "B"));
-        edgeSet.add(new Edge("B", "C"));
-        edgeSet.add(new Edge("C", "D"));
-        edgeSet.add(new Edge("D", "E"));
-        edgeSet.add(new Edge("A", "D"));
-        edgeSet.add(new Edge("A", "E"));
+        mapDirected.addVertex(0, "A");
+        mapDirected.addVertex(1, "B");
+        mapDirected.addVertex(2, "C");
+        mapDirected.addVertex(3, "D");
+        mapDirected.addVertex(4, "E");
 
-        mapDirected = new MapGraph(vertexSet, edgeSet, true, false);
-        mapUndirected = new MapGraph(vertexSet, edgeSet, false, false);
+        mapDirected.addEdge(0,1);
+        mapDirected.addEdge(1,2);
+        mapDirected.addEdge(2,3);
+        mapDirected.addEdge(3,4);
+        mapDirected.addEdge(0,3);
+        mapDirected.addEdge(0,4);
+
     }
 
     @Test
     void testKhansReturnSize() {
-        List<String> result = GraphAlgorithms.kahnsTopological(mapDirected);
-        assertEquals(mapDirected.numVertices(), result.size(),
+        List<Integer> result = GraphAlgorithms.kahnsBFS(mapDirected);
+        assertEquals(mapDirected.getNumVertices(), result.size(),
                 "Kahns result should include all vertices if no cycles exist.");
     }
 
     @Test
     void testCyclicException() {
-        assertThrows(IllegalStateException.class, () ->
-                GraphAlgorithms.kahnsTopological(mapUndirected),
-                "Undirected graphs create cycles and should throw exception.");
-    }
-
-    @Test
-    void testOneCyclicException() {
         // Add edge that will produce a cycle
-        edgeSet.add(new Edge("B", "A"));
-        MapGraph mapDirectedCyclic = new MapGraph(vertexSet, edgeSet, true, false);
+        mapDirected.addEdge(1,0);
         assertThrows(IllegalStateException.class, () ->
-                GraphAlgorithms.kahnsTopological(mapDirectedCyclic),
+                        GraphAlgorithms.kahnsBFS(mapDirected),
                 "Kahns topological sort method will not work on cyclic graphs.");
     }
 
     @Test
     void testKhansReturnSort() {
-        List<String> result = GraphAlgorithms.kahnsTopological(mapDirected);
-        List<String> sortedVertices = new ArrayList<>(mapDirected.getVertices());
+        List<Integer> result = GraphAlgorithms.kahnsBFS(mapDirected);
+        List<Integer> sortedVertices = new ArrayList<>(mapDirected.getVertices().keySet());
         Collections.sort(sortedVertices);
         assertEquals(sortedVertices, result,
                 "Edges in graph were purposely written so vertices would appear in alphabetical order if method is correct.");
@@ -67,10 +54,9 @@ class GraphAlgorithmsTest {
 
     @Test
     void testKhansDisconnectedVertex() {
-        vertexSet.add("F");
-        MapGraph mapDisconnectedVertex = new MapGraph(vertexSet, edgeSet, true, false);
-        List<String> result = GraphAlgorithms.kahnsTopological(mapDisconnectedVertex);
-        assertEquals(mapDisconnectedVertex.numVertices(), result.size(),
+        mapDirected.addVertex(5, "F");
+        List<Integer> result = GraphAlgorithms.kahnsBFS(mapDirected);
+        assertEquals(mapDirected.getNumVertices(), result.size(),
                 "Kahns topological sort should include all vertices - including those that are disconnected.");
     }
 }
